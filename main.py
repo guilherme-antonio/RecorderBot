3cb1160505e56bc53b4b89b10b732f0d30babdbf
guilherme-antonio <guioliveira99@gmail.com>
2021-09-22 16:44:27 -0300

    Removing cache

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-import discord
+from discord.ext import commands
+from discord.utils import get
 import json
 
-client = discord.Client()
+client = commands.Bot(command_prefix='!')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.command()
+async def record(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_clients, guild=ctx.guild)
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
 
-    if message.content.startswith('-s'):
-        await message.channel.send("Oi :P")
+    
 
+@client.command()
+async def clear(ctx, amount=5):
+    await ctx.channel.purge(limit=amount)
+    await ctx.send("Messages have been cleared")
 
 with open("token.json") as jsonFile:
     jsonObject = json.load(jsonFile)
