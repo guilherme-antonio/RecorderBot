693668d5acf4b641f61c8cead0c47f1bebaff043
guilherme-antonio <guioliveira99@gmail.com>
2021-10-28 17:29:14 -0300

    Creating class to play youtube music with history

--- main.py
+++ main.py
@@ -1,27 +1,52 @@
 from discord.ext import commands
 from discord.utils import get
 import json
+from YTDLSource import YTDLSource
 
-client = commands.Bot(command_prefix='!')
+class Music(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.listen_channel = None
+        self.history_channel = None
 
-@client.command()
-async def record(ctx):
-    channel = ctx.message.author.voice.channel
-    voice = get(client.voice_clients, guild=ctx.guild)
-    if voice and voice.is_connected():
-        await voice.move_to(channel)
-    else:
-        voice = await channel.connect()
+    @commands.command()
+    async def listen(self, ctx):
+        self.listen_channel = ctx.message.channel
+        await self.listen_channel.send('Listen channel setted')
 
-    
+    @commands.command()
+    async def history(self, ctx):
+        self.history_channel = ctx.message.channel
+        await self.history_channel.send('History channel setted')
 
-@client.command()
-async def clear(ctx, amount=5):
-    await ctx.channel.purge(limit=amount)
-    await ctx.send("Messages have been cleared")
+    @commands.command()
+    async def clear(self, ctx, amount=5):
+        await ctx.channel.purge(limit=amount)
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+                return
+
+        if (self.listen_channel is not None and message.channel.id == self.listen_channel.id and message.content != '!listen'):
+            channel = message.author.voice.channel
+            voice = get(self.bot.voice_clients, guild=message.guild)
+            if voice and voice.is_connected():
+                await voice.move_to(channel)
+            else:
+                voice = await channel.connect()
+
+            player = await YTDLSource.from_url(message.content, loop=False)
+
+            voice.play(player)
+
+            if (self.history_channel is not None):
+                await self.history_channel.send('Listening')
 
 with open("token.json") as jsonFile:
     jsonObject = json.load(jsonFile)
     jsonFile.close()
 
-client.run(jsonObject['token'])
+bot = commands.Bot(command_prefix='!')
+bot.add_cog(Music(bot))
+bot.run(jsonObject['token'])
